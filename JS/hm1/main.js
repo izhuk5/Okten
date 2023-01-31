f830967203d9f952dc8408bcab25155ab9395e27
izhuk5 <ihorzhuk55555@icloud.com>
2023-01-31 21:44:27 +0200

    changes

---
+++ JS/hm1/main.js
@@ -0,0 +1,48 @@
+// -  1. Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+// Вивести кожну змінну за допомогою: console.log
+//
+// -  2. Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person (Не об'єкт, просто за допомоги конкатенації)
+//
+// -  3. За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+//   let a = 100; let b = '100'; let c = true;
+//
+//  - 4. Додаткове для тих хто цікавився prompt`oм
+// - За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками. та вивести в консоль
+
+// 1.
+// let greetings = 'hello';
+// let owu = 'owu';
+// let urlCom = 'com';
+// let urlUa = 'ua';
+// let a = 1;
+// let b = 10;
+// let c = 999;
+// let d = 123;
+// let e = 3.14;
+// let f = 2.7;
+// let j = 16;
+// let h = true;
+// let m = false;
+
+// 2.
+// let firstName = 'Ihor';
+// let middleName = 'Ihorevich';
+// let lastName = 'Zhuk';
+//
+// let person = `${firstName} ${middleName} ${lastName}`;
+// console.log(person);
+
+// 3.
+// let a = 100;
+// let b = '100';
+// let c = true;
+//
+// console.log(typeof a,b,c);
+
+// 4.
+// let askYourFirstName = prompt('Ваше імя', '');
+// let askYourPatronymicName = prompt('По батькові', '');
+// let askYourAge = +prompt('Який ваш вік?', '');
+// let person = `${askYourFirstName} ${askYourPatronymicName} ${askYourAge}`;
+//
+// console.log(person);
